c8b514ef0104430606e8492f6a1ce8431eeb5d11
Florian Verdonck <Florian Verdonck>
2016-11-21 22:05:09 +0100

    Adding and removing users + setting up JavaScript interopt

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,62 +1,159 @@
+port module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
-
-
--- APP
-main : Program Never Int Msg
-main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+import Html.Events exposing (..)
+import Dict exposing (Dict)
+import Debug
 
 
--- MODEL
-type alias Model = Int
-
-model : number
-model = 0
+port generationRequest : List Int -> Cmd msg
 
 
--- UPDATE
-type Msg = NoOp | Increment
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+port generationResponse : (List Int -> msg) -> Sub msg
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+type alias Person =
+    { name : String, id : Int }
+
+
+type alias Model =
+    { people : List Person, result : Dict Int Int, newPerson : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( (Model [ Person "Florian" 1, Person "Jochen" 2, Person "Korneel" 3 ] Dict.empty ""), Cmd.none )
+
+
+type Msg
+    = UpdateNewPerson String
+    | AddPerson
+    | RemovePerson Int
+    | Generate
+    | Result (List Int)
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div []
+        [ createForm model.newPerson
+        , createPeopleTable model.people
+        , createGenerateButton model.people
         ]
-      ]
-    ]
-  ]
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+formButtonClass : String -> String
+formButtonClass currentName =
+    if String.isEmpty currentName then
+        "button is-info is-disabled"
+    else
+        "button is-info"
+
+
+createForm : String -> Html Msg
+createForm currentName =
+    Html.form [ class "entry box", onSubmit AddPerson ]
+        [ h4 [ class "subtitle" ] [ text "Personen toevoegen" ]
+        , p [ class "control has-addons" ]
+            [ input [ class "input", type_ "text", placeholder "Vul een nieuwe naam in", onInput UpdateNewPerson, value currentName ] []
+            , a [ class (formButtonClass currentName), onClick AddPerson ] [ text "Toevoegen" ]
+            ]
+        ]
+
+
+createPeopleTable : List Person -> Html Msg
+createPeopleTable people =
+    table [ class "table is-striped" ]
+        [ thead []
+            [ tr []
+                [ th [] [ text "Naam" ]
+                , th [ colspan 3 ] [ text "Verwijderen" ]
+                ]
+            ]
+        , tbody [] (List.map createPeopleTableRow people)
+        ]
+
+
+createPeopleTableRow : Person -> Html Msg
+createPeopleTableRow person =
+    tr []
+        [ td [] [ text person.name ]
+        , td [ class "is-icon" ]
+            [ div [ class "button is-danger is-small", onClick (RemovePerson person.id) ]
+                [ i
+                    [ class "fa fa-times" ]
+                    []
+                ]
+            ]
+        ]
+
+
+createGenerateButton : List Person -> Html Msg
+createGenerateButton people =
+    let
+        disabledClass =
+            if List.length people <= 1 then
+                "is-disabled"
+            else
+                ""
+    in
+        button [ class ("button is-primary is-large " ++ disabledClass), onClick Generate ] [ text "Genereer" ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateNewPerson newValue ->
+            ( { model | newPerson = newValue }, Cmd.none )
+
+        AddPerson ->
+            addPerson model
+
+        RemovePerson id ->
+            ( { model | people = (List.filter (\p -> p.id /= id) model.people) }, Cmd.none )
+
+        Generate ->
+            ( model, generationRequest (List.map (\p -> p.id) model.people) )
+
+        Result ids ->
+            let
+                foo =
+                    Debug.log "from JS" ids
+            in
+                ( model, Cmd.none )
+
+
+addPerson : Model -> ( Model, Cmd Msg )
+addPerson model =
+    let
+        nextId =
+            List.map (\p -> p.id) model.people
+                |> List.maximum
+                |> Maybe.withDefault 0
+                |> (+) 1
+
+        nextPerson =
+            Person model.newPerson nextId
+    in
+        ( { model | people = nextPerson :: model.people, newPerson = "" }, Cmd.none )
+
+
+generateOrder : Model -> ( Model, Cmd Msg )
+generateOrder model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    generationResponse Result
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
